03aa37bb6fe172ec49ca04578d584dc999f41aa0
Arthur <tutusobreira@gmail.com>
2023-02-01 20:39:51 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ let mountains_behind = document.getElementById('mountains_behind');
 let mountains_front = document.getElementById('mountains_front');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_front.style.left = value * 1.05 + 'px'
+})
